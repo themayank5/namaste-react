027d9979467f94e531e164443af308f7fc059e35
mayank raj <mayank60.singh>
2024-04-04 10:13:58 +0530

    episode3 done

--- app.js
+++ app.js
@@ -2,35 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"this is h1 tag from child1"),
-        React.createElement("h2",{},"this is h2 tag from child1")
-       ]
-      
-       ),
-       React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"this is h1 tag from child2"),
-        React.createElement("h2",{},"this is h2 tag from child2")
-       ]
-      
-       ),
-       React.createElement("div",{id:"child3"},[
-        React.createElement("h1",{},"this is h1 tag from child3"),
-        React.createElement("h2",{},"this is h2 tag from child3")
-       ]
-      
-       ),
-       React.createElement("div",{id:"child4"},[
-        React.createElement("h1",{},"this is h1 tags from child4"),
-        React.createElement("h2",{},"this is h2 tagss from child4")
-       ]
-      
-       )
-]
-               
-)
+const Tittle= () => {
+    return(
+        <h1>this is tittle component</h1>
+    )
+}
+
+const HeadingComponent = () => {
+    return(
+        <div>
+         <Tittle />
+        <h1>hello mayank singh</h1>
+        </div>
+
+        
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent/>);
